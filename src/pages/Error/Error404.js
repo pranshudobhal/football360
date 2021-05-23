3454faf288d6a3810d7ada18d6eba0a5877e0a68
Pranshu Dobhal <pranshu.dobhal@gmail.com>
2021-05-23 22:45:56 +0530

    updated Error404 Page

--- src/pages/Error/Error404.js
+++ src/pages/Error/Error404.js
@@ -1,8 +1,16 @@
+import { useNavigate } from 'react-router-dom';
+import styles from './Error404.module.css';
+
 export function Error404() {
+  const navigate = useNavigate();
   return (
-    <>
-      <h1>Error 404</h1>
-      <p>Please check your URL</p>
-    </>
+    <div className={styles.errorContainer}>
+      <div className={styles.error}>
+        <h1>404</h1>
+        <h2>Page Not Found!</h2>
+        <p>The page you are looking for doesn't exist or any other error occured.</p>
+        <button onClick={() => navigate('/')}>Go to Homepage</button>
+      </div>
+    </div>
   );
 }
